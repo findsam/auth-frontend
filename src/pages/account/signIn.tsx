935b97ba978b5bbc7180b5014de906dfd1855ba1
sam <84742666+findsam@users.noreply.github.com>
2024-08-17 15:02:09 +0100

    added signin boilerplate

--- src/pages/account/signIn.tsx
+++ src/pages/account/signIn.tsx
@@ -1,5 +1,41 @@
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { SignInInputs } from "~/libs/types";
+
 const SignIn: React.FC = () => {
-  return <div>123</div>;
+  const methods = useForm<SignInInputs>({
+    resolver: joiResolver(
+      Joi.object({
+        email: Joi.string()
+          .email({ tlds: { allow: false } })
+          .required()
+          .label("Email"),
+        password: Joi.string().max(32).required().label("Password"),
+      }).messages({
+        "string.empty": "{#label} is required",
+      })
+    ),
+  });
+
+  const login: SubmitHandler<SignInInputs> = async (values: SignInInputs) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form
+        style={{ display: "grid", maxWidth: "270px", gap: "6px" }}
+        onSubmit={methods.handleSubmit(login)}
+      >
+        <label>email</label>
+        <input {...methods.register("email")} />
+        <label>password</label>
+        <input {...methods.register("password")} />
+        <button type="submit">sign-in</button>
+      </form>
+    </FormProvider>
+  );
 };
 
 export default SignIn;
